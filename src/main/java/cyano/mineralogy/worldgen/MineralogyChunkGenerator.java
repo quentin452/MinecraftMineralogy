9aee2be77eed56a6575efb3ec8640b81bd60fa22
QUENTIN453 <quentinlamauve@gmail.com>
2023-09-07 09:45:58 +0200

    Massive Performance improvements

--- src/main/java/cyano/mineralogy/worldgen/MineralogyChunkGenerator.java
+++ src/main/java/cyano/mineralogy/worldgen/MineralogyChunkGenerator.java
@@ -1,12 +1,5 @@
 package cyano.mineralogy.worldgen;
 
-import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.CAVE;
-import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.MINESHAFT;
-import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.RAVINE;
-import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.SCATTERED_FEATURE;
-import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.STRONGHOLD;
-import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.VILLAGE;
-
 import java.util.List;
 import java.util.Random;
 
@@ -15,6 +8,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.ChunkProviderGenerate;
 import net.minecraft.world.gen.MapGenBase;
 import net.minecraft.world.gen.MapGenCaves;
 import net.minecraft.world.gen.MapGenRavine;
@@ -22,19 +16,10 @@ import net.minecraft.world.gen.structure.MapGenMineshaft;
 import net.minecraft.world.gen.structure.MapGenScatteredFeature;
 import net.minecraft.world.gen.structure.MapGenStronghold;
 import net.minecraft.world.gen.structure.MapGenVillage;
-import net.minecraftforge.event.terraingen.TerrainGen;
 
 import cyano.mineralogy.Mineralogy;
-import cyano.mineralogy.worldgen.gen.ChunkProviderGenerateCustom;
 
-/**
- * Replaces minecraft generic stone with new mineralogy stone blocks
- *
- * @author Cyanobacterium
- *
- */
-
-public class MineralogyChunkGenerator extends ChunkProviderGenerateCustom {
+public class MineralogyChunkGenerator extends ChunkProviderGenerate {
 
     final String generatorOptionsString;
     final WorldType worldType;
@@ -42,6 +27,14 @@ public class MineralogyChunkGenerator extends ChunkProviderGenerateCustom {
     final Geology geome;
     final boolean mapFeaturesEnabled;
     final Random rand;
+
+    private MapGenBase caveGenerator = new MapGenCaves();
+    private MapGenStronghold strongholdGenerator = new MapGenStronghold();
+    private MapGenVillage villageGenerator = new MapGenVillage();
+    private MapGenMineshaft mineshaftGenerator = new MapGenMineshaft();
+    private MapGenScatteredFeature scatteredFeatureGenerator = new MapGenScatteredFeature();
+    private MapGenBase ravineGenerator = new MapGenRavine();
+    private BiomeGenBase[] biomesForGeneration;
 
     public MineralogyChunkGenerator(World world, long seed, boolean mapFeaturesEnabled, String generatorOptionsString,
         WorldType worldType) {
@@ -55,41 +48,19 @@ public class MineralogyChunkGenerator extends ChunkProviderGenerateCustom {
         geome = new Geology(seed, Mineralogy.GEOME_SIZE, Mineralogy.ROCK_LAYER_SIZE);
     }
 
-    private MapGenBase caveGenerator = new MapGenCaves();
-    /** Holds Stronghold Generator */
-    private MapGenStronghold strongholdGenerator = new MapGenStronghold();
-    /** Holds Village Generator */
-    private MapGenVillage villageGenerator = new MapGenVillage();
-    /** Holds Mineshaft Generator */
-    private MapGenMineshaft mineshaftGenerator = new MapGenMineshaft();
-    private MapGenScatteredFeature scatteredFeatureGenerator = new MapGenScatteredFeature();
-    /** Holds ravine generator */
-    private MapGenBase ravineGenerator = new MapGenRavine();
-    /** The biomes that are used to generate the chunk */
-    private BiomeGenBase[] biomesForGeneration;
-    {
-        caveGenerator = TerrainGen.getModdedMapGen(caveGenerator, CAVE);
-        strongholdGenerator = (MapGenStronghold) TerrainGen.getModdedMapGen(strongholdGenerator, STRONGHOLD);
-        villageGenerator = (MapGenVillage) TerrainGen.getModdedMapGen(villageGenerator, VILLAGE);
-        mineshaftGenerator = (MapGenMineshaft) TerrainGen.getModdedMapGen(mineshaftGenerator, MINESHAFT);
-        scatteredFeatureGenerator = (MapGenScatteredFeature) TerrainGen
-            .getModdedMapGen(scatteredFeatureGenerator, SCATTERED_FEATURE);
-        ravineGenerator = TerrainGen.getModdedMapGen(ravineGenerator, RAVINE);
-    }
-
     @Override
-    public Chunk provideChunk(int chunkX, int chunkY) {
-        this.rand.setSeed((long) chunkX * 341873128712L + (long) chunkY * 132897987541L);
-        Block[] ablock = new Block[65536];
+    public Chunk provideChunk(int chunkX, int chunkZ) {
+        this.rand.setSeed((long) chunkX * 341873128712L + (long) chunkZ * 132897987541L);
+        Block[] blocks = new Block[65536];
         byte[] abyte = new byte[65536];
-        this.func_147424_a(chunkX, chunkY, ablock);
+        this.func_147424_a(chunkX, chunkZ, blocks);
         this.biomesForGeneration = this.worldObj.getWorldChunkManager()
-            .loadBlockGeneratorData(this.biomesForGeneration, chunkX * 16, chunkY * 16, 16, 16);
-        this.replaceBlocksForBiome(chunkX, chunkY, ablock, abyte, this.biomesForGeneration);
-        this.caveGenerator.func_151539_a(this, this.worldObj, chunkX, chunkY, ablock);
-        this.ravineGenerator.func_151539_a(this, this.worldObj, chunkX, chunkY, ablock);
+            .getBiomesForGeneration(this.biomesForGeneration, chunkX * 16, chunkZ * 16, 16, 16);
+        this.replaceBlocksForBiome(chunkX, chunkZ, blocks, abyte, this.biomesForGeneration);
+        this.caveGenerator.func_151539_a(this, this.worldObj, chunkX, chunkZ, blocks);
+        this.ravineGenerator.func_151539_a(this, this.worldObj, chunkX, chunkZ, blocks);
 
-        geome.replaceStoneInChunk(chunkX, chunkY, ablock);
+        geome.replaceStoneInChunk(chunkX, chunkZ, blocks);
 
         if (this.mapFeaturesEnabled) {
             this.mineshaftGenerator.func_143025_a();
@@ -98,7 +69,7 @@ public class MineralogyChunkGenerator extends ChunkProviderGenerateCustom {
             this.scatteredFeatureGenerator.func_143025_a();
         }
 
-        Chunk chunk = new Chunk(this.worldObj, ablock, abyte, chunkX, chunkY);
+        Chunk chunk = new Chunk(this.worldObj, blocks, abyte, chunkX, chunkZ);
         byte[] abyte1 = chunk.getBiomeArray();
 
         for (int k = 0; k < abyte1.length; ++k) {
@@ -109,59 +80,17 @@ public class MineralogyChunkGenerator extends ChunkProviderGenerateCustom {
         return chunk;
     }
 
-    /*
-     * @Override
-     * public void generate(Random random, int chunkX, int chunkZ, World world,
-     * IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-     * Abstract3FCoordinateNoiseGenerator perlinNoise = new Default3FCoordinateNoiseGenerator(new
-     * Default3CoordinatePRNG(world.getSeed(),-1f,1f),Mineralogy.ROCK_LAYER_SIZE);
-     * // Implementation is WAY TOO SLOW!
-     * if(world.provider.dimensionId == 0){ // only apply to overworld, not Nether or others
-     * for(int x = chunkX << 4; x < ((chunkX << 4) + 16); x++){
-     * for(int z = chunkZ << 4; z < ((chunkZ << 4) + 16); z++){
-     * for(int y = world.getHeightValue(x, z); y > 50; y--){
-     * if(world.getBlock(x, y, z) == Blocks.stone ){
-     * // replace "stone" with a real stone
-     * // TODO use geo-biomes (aka geomes)
-     * world.setBlock(x,y,z,pickBlockFromList(perlinNoise.getValue(x, y*Mineralogy.Y_SCALE_FACTOR,
-     * z),Mineralogy.igneousStones));
-     * }
-     * }
-     * }
-     * }
-     * // TODO
-     * }
-     * }
-     */
-
-    /**
-     * given any number, this method grabs a block from the list based on that number.
-     * A gradually increasing number would cycle through the list.
-     *
-     * @param value
-     * @param list
-     * @return
-     */
     static Block pickBlockFromList(float value, List<Block> list) {
         float w = value - floor(value);
         int index = (int) (w * list.size());
         return list.get(index);
     }
 
-    /**
-     * Faster implementation than Math.floor(x).
-     *
-     * @param x
-     * @return The greatest integer value less than x. If x is NaN, then 0 is
-     *         returned. If x is infinite (positive or negative), then Long.MAX_VALUE is returned.
-     */
     private static int floor(float x) {
-        // return (long)Math.floor(x);/*
         if (x < 0) {
             return (int) x - 1;
         } else {
             return (int) x;
-        } /* */
+        }
     }
-
 }
